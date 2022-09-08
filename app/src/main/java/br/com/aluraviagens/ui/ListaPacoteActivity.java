f8afd88843a3a261adfbd9fb5c98f118d3ebac13
Pedro <phptrebil@gmail.com>
2022-09-08 17:19:53 -0300

    first commit

---
+++ app/src/main/java/br/com/aluraviagens/ui/ListaPacoteActivity.java
@@ -0,0 +1,16 @@
+package br.com.aluraviagens.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.aluraviagens.R;
+
+public class ListaPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_pacotes);
+    }
+}
